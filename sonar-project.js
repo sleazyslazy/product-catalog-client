36832837e741ad720d0685279b943cb9b0dd8117
developer <gnunn@redhat.com>
2020-04-09 12:38:08 -0700

    Add dependency-check

--- sonar-project.js
+++ sonar-project.js
@@ -8,7 +8,9 @@ sonarqubeScanner(
             'sonar.tests':  'src',
             'sonar.inclusions'  :  '**', // Entry point of your code
             'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
-            'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info'
+            'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
+            'sonar.dependencyCheck.jsonReportPath': 'reports/dependency-check/dependency-check-report.json',
+            'sonar.dependencyCheck.htmlReportPath': 'reports/dependency-check/dependency-check-report.html'
 //            'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml'
         }
     }, () => {});
